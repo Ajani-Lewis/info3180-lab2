03947386942f7c4943b89febd4a5e88c540fcf43
Ajani-Lewis <ajani.j.lewis@gmail.com>
2022-02-13 19:18:53 -0500

    Edited views.py

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Ajani Lewis")
 
 @app.route('/profile')
 def profile():
